faf81f98f870841e879f7bc8a27e3f78aff8ae08
Indhu-AR <155819612+Indhu-AR@users.noreply.github.com>
2026-06-10 14:42:05 +0530

    Add files via upload

---
+++ throwusingunchecked.java
@@ -0,0 +1,15 @@
+package javaclass;
+
+public class throwusingunchecked {
+	
+		public static void main(String[] args) {
+			int age = 15;
+			if(age<18) {
+				throw new ArithmeticException("Not Eligible");
+			}
+			System.out.println("Eligible");
+
+		}
+
+	}
+
