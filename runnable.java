d02b0cd91e095b4e9770b422789faf9bf96d3bed
Indhu-AR <155819612+Indhu-AR@users.noreply.github.com>
2026-06-13 15:39:14 +0530

    Add files via upload

---
+++ runnable.java
@@ -0,0 +1,12 @@
+package Multithreading;
+
+public class runnable implements Runnable {
+public void run() {
+	System.out.println("Thread is running");
+}
+public static void main(String[] args) {
+		Mythread t = new Mythread();
+		Thread t1=new Thread(t);
+        t1.start();
+}
+}
