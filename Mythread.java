4633ad6dfddc1db7c129bbb2a11183927909c622
Indhu-AR <155819612+Indhu-AR@users.noreply.github.com>
2026-06-13 15:38:36 +0530

    Add files via upload

--- Mythread.java
+++ Mythread.java
@@ -1,16 +1,14 @@
-package javaclass;
+package Multithreading;
 
 public class Mythread extends Thread{
-	public void run() {
-		System.out.println("Thread is running");
-	}
+public void run() {
+	System.out.println("Thread is running");
+}
 
-		public static void main(String[] args) {
-			Mythread t = new Mythread();
-	        t.start();
-
-		}
-	
+	public static void main(String[] args) {
+		Mythread t = new Mythread();
+        t.start();
 
 	}
 
+	}
